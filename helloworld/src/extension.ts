e57ed1a492d9a090cae295ed26f0af6440426361
Jalbin1307 <az13077@gmail.com>
2021-10-14 12:01:32 +0900

    feat : Add a new command(txtconvert)

--- helloworld/src/extension.ts
+++ helloworld/src/extension.ts
@@ -11,20 +11,18 @@ vscode.languages.registerHoverProvider('onnx',{
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "helloworld" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
+
 	let disposable = vscode.commands.registerCommand('helloworld.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from HelloWorld!');
 	});
-
 	context.subscriptions.push(disposable);
+
+	let convert = vscode.commands.registerCommand('helloworld.txtconvert', () => {
+        vscode.window.showInformationMessage('txt convert!');
+    });
+	context.subscriptions.push(convert);
 }
 
 // this method is called when your extension is deactivated
