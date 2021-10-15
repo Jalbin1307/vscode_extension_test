e5cfb7b61cc46c272cceb6e2a12c1ded9cf5fc8a
Jalbin1307 <az13077@gmail.com>
2021-10-15 16:44:36 +0900

    feat : Add angular

--- helloworld/src/extension.ts
+++ helloworld/src/extension.ts
@@ -1,17 +1,20 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
-vscode.languages.registerHoverProvider('onnx',{
-	provideHover(document,position,token){
-		return new vscode.Hover("I am onnx");
-	}
-});
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+// vscode.languages.registerHoverProvider('onnx',{
+// 	provideHover(document,position,token){
+// 		return new vscode.Hover("I am onnx");
+// 	}
+// });
+
 export function activate(context: vscode.ExtensionContext) {
 	
 	console.log('Congratulations, your extension "helloworld" is now active!');
+	
 
 
 	let disposable = vscode.commands.registerCommand('helloworld.helloWorld', () => {
