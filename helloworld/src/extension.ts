f0d9c9cf7a3043270c31e801abeaebfb0e8eacc9
Jalbin1307 <az13077@gmail.com>
2021-10-26 22:27:05 +0900

    style : Clean up code

--- helloworld/src/extension.ts
+++ helloworld/src/extension.ts
@@ -1,18 +1,10 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import {Uri} from 'vscode';
 import { request } from 'http';
-import { createWriteStream } from 'fs';
  
-const fileStream = createWriteStream('.\request2.txt');   // Extension 사용자의 폴더에 저장할 수 있는 방법 찾아봐야함 +현재는 절대 경로로만 가능
-
-
-
 export function activate(context: vscode.ExtensionContext) {
 	
 	console.log('Extension is now active!');	
-
 
 	let disposable = vscode.commands.registerCommand('helloworld.helloWorld', () => {
 		vscode.window.showInformationMessage('Hello World');
@@ -57,12 +49,6 @@ export function activate(context: vscode.ExtensionContext) {
 
 	
 	let convert = vscode.commands.registerCommand('helloworld.txtconvert', (args : any) => {
-		const filePath: string | undefined = args.filePath;
-		const file = filePath ? Uri.file(filePath) : undefined;
-		vscode.window.showInformationMessage('File Path : '+args);
-		//const fileStream = createWriteStream(filePath+"\\request.txt");
-
-		// vscode.window.showInformationMessage(fileP);
     
     });
 	context.subscriptions.push(convert);
