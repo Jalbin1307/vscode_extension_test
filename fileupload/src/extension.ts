e8d17340379d9861f22ca0b135d01b3c31e672ff
Jalbin1307 <az13077@gmail.com>
2021-11-01 15:43:51 +0900

    feat : Add axios command

--- fileupload/src/extension.ts
+++ fileupload/src/extension.ts
@@ -1,26 +1,59 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as FormData from 'form-data';
+import axios from "axios";
+import { createReadStream} from 'fs';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+
 export function activate(context: vscode.ExtensionContext) {
 	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
+	
 	console.log('Congratulations, your extension "fileupload" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('fileupload.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
+	
+	let disposable = vscode.commands.registerCommand('fileupload.helloWorld', async () => {
+		const url = "127.0.0.1:8000/rest_api_test/";
+		axios.get("url")
+    	.then(function (response) {
+			vscode.window.showInformationMessage('Hello World from Fileupload!');
+         // response  
+    	}).catch(function (error) {
+			vscode.window.showInformationMessage('Error');
+        // 오류발생시 실행
+    	}).then(function() {
+        // 항상 실행
+    	});
+
+		try {
+			const data = await axios.get("url");
+		} catch {
+			// 오류 발생시 실행
+			vscode.window.showInformationMessage('Error2');
+		}
+
 		vscode.window.showInformationMessage('Hello World from Fileupload!');
 	});
-
 	context.subscriptions.push(disposable);
+
+
+	let req = vscode.commands.registerCommand('fileupload.req', () => {
+		const headers = {'userID':'1234'};
+		var bodyFromData = new FormData();
+		//await bodyFromData.append('processID':'1234');
+		bodyFromData.append('file', createReadStream("/Users/hongjin-u/Documents/libe.xlsx"), "libe.xlsx");
+		const url = "127.0.0.1:8000/rest_api_test/";
+
+		axios.post(url,
+			 bodyFromData,
+			 {headers:{'Content-Type': 'multipart/form-data'}});
+
+		//console.log(result);
+
+
+		vscode.window.showInformationMessage('Request!');
+	});
+
+	context.subscriptions.push(req);
 }
 
-// this method is called when your extension is deactivated
+
 export function deactivate() {}
