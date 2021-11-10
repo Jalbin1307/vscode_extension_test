9f507a262ed1b6da2557f96366b44da2b2bee9be
Jalbin1307 <az13077@gmail.com>
2021-11-10 18:40:30 +0900

    ERROR

--- agu/src/extension.ts
+++ agu/src/extension.ts
@@ -1,6 +1,6 @@
 import * as vscode from 'vscode';
 // import fetch, { Headers, RequestInit } from "node-fetch";
-import FormData = require("form-data");
+import * as FormData from 'form-data';
 import * as fs from 'fs';
 import * as path from 'path';
 import { Meteor } from 'meteor/meteor';
