9a6fadad31d0243e9faf54ad73524b2fcab54fe3
Jalbin1307 <az13077@gmail.com>
2021-11-24 21:34:35 +0900

    feat(axios-upload) : ASYNC ERROR

--- axios-upload/src/commands/upload.ts
+++ axios-upload/src/commands/upload.ts
@@ -1,18 +1,48 @@
 import { window, workspace, ExtensionContext, Uri } from "vscode";
 import * as fs from "fs-extra";
 import { sendFormData } from "../utils/sendFormData";
+import { Stream } from "stream";
+import * as FormData from "form-data";
+import axios from "axios";
+import { axiospost } from "../utils/axiosPost";
 
-export async function upload(context: ExtensionContext, items: Uri[], opts: any = {}) {
+export function upload(context: ExtensionContext, items: Uri[], opts: any = {}) {
     let axiosRes;
-
     const wsConfig = workspace.getConfiguration("axios-upload");
-    
-    const resources = items.map((uri) => uri.path);
     
     let filePath = items[0].path;
 
-    // axiosRes = await sendFormData(filePath,resources);
-    let stream = await sendFormData(filePath, resources);
-    console.log(stream);
-    console.log('123');
+    const url = 'http://127.0.0.1:8000/rest_api_test/';
+
+    const form = new FormData();
+    const resources = items.map((uri) => uri.path);
+    
+    const st = async ()=>{
+        return fs.createReadStream(filePath);
+    };
+    const stReturn = async () => {
+        const stream = await st();
+        console.log(stream);
+        
+        form.append('file',stream);
+        
+        console.log(form);
+        return stream;
+    };
+
+    // let stream = await sendFormData(filePath, resources);
+    // const stream : Stream  = fs.createReadStream(filePath);
+    stReturn().then(value=>{
+        console.log(value);
+        const axiosRequestConfig = {
+            // headers: {...form.getHeaders() },
+            //timeout: 5000,
+          };
+        //const res = axios.post(url, value, axiosRequestConfig);
+        // console.log(form);
+        //axios.post(url, form, axiosRequestConfig);
+        //const res2 = axios.post(url, value, axiosRequestConfig);
+    })
+
+    console.log("123");
 }
