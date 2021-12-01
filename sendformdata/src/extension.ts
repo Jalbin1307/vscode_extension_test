c9c3829028cf7c25b728229e3047308bfd49aea3
Jalbin1307 <az13077@gmail.com>
2021-12-01 17:47:00 +0900

    feat : Save a response zipfile

--- sendformdata/src/extension.ts
+++ sendformdata/src/extension.ts
@@ -1,8 +1,8 @@
 import * as vscode from 'vscode';
 import { Uri } from 'vscode';
 import FormData = require('form-data');
-
 const fs = require('fs');
+
 // var XMLHttpRequest = require('xhr2');
 var request = require('request');
 
@@ -13,6 +13,8 @@ export function activate(context: vscode.ExtensionContext) {
 
 	let disposable = vscode.commands.registerCommand('sendformdata.helloWorld', (uri: Uri, items : Uri[]) => {
 
+		// fs.createWriteStream("/Users/hongjin-u/onnx_test_f/test.zip");
+
 		var formData = new FormData();
 		// formData.append("foo","123");
 		// formData.append("acount",123456);
@@ -21,8 +23,29 @@ export function activate(context: vscode.ExtensionContext) {
 
 		//http://127.0.0.1:8000/
 		//https://mysite-tscvl.run.goorm.io/
-		formData.submit('https://mysite-tscvl.run.goorm.io/rest_api_test/', function(err, res){
+		formData.submit('http://127.0.0.1:8000/rest_api_test/', function(err, res){
 			if(err) throw err;
+			if(res){
+				
+				console.log(res);
+				console.log("RES 받음");
+				var wstream = fs.createWriteStream("/Users/hongjin-u/onnx_test_f/test.zip");
+				// fs.writeFile("/Users/hongjin-u/onnx_test_f/connx.zip", res);
+				res.on('data',function(data){
+					wstream.write(data);
+				});
+				res.on('end',function(){
+					wstream.end();
+				});
+				res.on('error',function(err){
+					console.log('something is wrong');
+					wstream.close();
+				});
+
+				console.log("RES 성공");
+				
+				
+			}
 
 			console.log('Done');
 		});
